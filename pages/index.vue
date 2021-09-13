87ae7c7ce1a0089827b3910839e9d5c37359e2b9
Jean Carlos Ayala <ayalajeancarlos.91@gmail.com>
2021-09-13 14:44:29 +0100

    chore: test gun db

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,52 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">cat-colonies-map</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <form @click.prevent="addColony()">
+      <input v-model="newColony.name" type="text" />
+      <input v-model.number="newColony.aproxPopulation" type="text" />
+      <button type="submit">crear colonia</button>
+    </form>
+    <ul>
+      <li v-for="(colony, key) in colonies" :key="key">
+        <pre>{{ colony }}</pre>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      newColony: {
+        name: '',
+        aproxPopulation: 0,
+      },
+      colonies: {},
+    }
+  },
   mounted() {
     this.$gun
       .get('cat-colonies')
       .map()
       .on((node, key) => {
-        // add results straight to the Vue component state
-        // and get updates when nodes are updated by GUN
-        this.vueState[key] = node
+        if (node !== null) {
+          this.colonies = {
+            ...this.colonies,
+            [key]: { ...node },
+          }
+        }
       })
+  },
+  methods: {
+    addColony() {
+      const colonyName = this.newColony.name
+      const aproxPopulation = this.newColony.aproxPopulation
+      const newColony = this.$gun.get(colonyName).put({
+        name: colonyName,
+        'aprox-population': aproxPopulation,
+      })
+      this.$gun.get('cat-colonies').set(newColony)
+    },
   },
 }
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
