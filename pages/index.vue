1a489f9d8036851e6116943cc0d473d5acea2936
Jean Carlos Ayala <ayalajeancarlos.91@gmail.com>
2021-09-13 20:02:30 +0100

    feat: save colony location

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <form @click.prevent="addColony()">
-      <input v-model="newColony.name" type="text" />
-      <input v-model.number="newColony.aproxPopulation" type="text" />
-      <button type="submit">crear colonia</button>
+    <form action="" @submit.prevent="addColony()">
+      <input v-model="newColony.name" type="text" required />
+      <input v-model.number="newColony.aproxPopulation" type="number" />
+      <button type="submit">
+        {{ addingColony ? 'registrando...' : 'registrar colonia' }}
+      </button>
     </form>
+    <p v-show="gettingLocation">Consiguiendo ubicación...</p>
     <ul>
       <li v-for="(colony, key) in colonies" :key="key">
-        <pre>{{ colony }}</pre>
+        <pre>{{ colony.name }}</pre>
       </li>
     </ul>
   </div>
@@ -17,6 +20,8 @@
 export default {
   data() {
     return {
+      gettingLocation: false,
+      addingColony: false,
       newColony: {
         name: '',
         aproxPopulation: 0,
@@ -30,22 +35,59 @@ export default {
       .map()
       .on((node, key) => {
         if (node !== null) {
-          this.colonies = {
-            ...this.colonies,
-            [key]: { ...node },
+          if (node.location) {
+            this.$gun.get(`${key}/location`).once(
+              (location) =>
+                (this.colonies = {
+                  ...this.colonies,
+                  [key]: {
+                    ...node,
+                    location,
+                  },
+                })
+            )
           }
         }
       })
   },
   methods: {
-    addColony() {
+    getGeolocation() {
+      return new Promise((resolve, reject) => {
+        if (!('geolocation' in navigator)) {
+          reject(new Error('Geolocalización no disponible.'))
+        }
+
+        this.gettingLocation = true
+
+        navigator.geolocation.getCurrentPosition(
+          (pos) => {
+            this.gettingLocation = false
+            resolve(pos)
+          },
+          (err) => {
+            this.gettingLocation = false
+            reject(err)
+          }
+        )
+      })
+    },
+    async addColony() {
+      this.addingColony = true
+      const pos = await this.getGeolocation()
+      const lat = pos.coords.latitude
+      const long = pos.coords.longitude
       const colonyName = this.newColony.name
       const aproxPopulation = this.newColony.aproxPopulation
       const newColony = this.$gun.get(colonyName).put({
         name: colonyName,
         'aprox-population': aproxPopulation,
+        location: {
+          lat,
+          long,
+        },
       })
       this.$gun.get('cat-colonies').set(newColony)
+      this.addingColony = false
     },
   },
 }
