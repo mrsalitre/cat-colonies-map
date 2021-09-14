94e8e39b622d880061be59c1208844cf6a1da5cd
Jean Carlos Ayala <ayalajeancarlos.91@gmail.com>
2021-09-14 23:14:35 +0100

    chore: vue-image-uploader instalation

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/vue-gun.client.js'],
+  plugins: [
+    '~/plugins/vue-gun.client.js',
+    '~/plugins/vue-image-uploader.client.js',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/vue-image-uploader.client.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import ImageUploader from 'vue-image-upload-resize'
+Vue.use(ImageUploader)
